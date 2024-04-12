977e6c7630c6e937de743b22c930199ab78280cc
yu-fukunaga <92198819+yu-fukunaga@users.noreply.github.com>
2024-04-12 23:16:58 +0900

    import storybook/test

--- src/components/InboxScreen.stories.jsx
+++ src/components/InboxScreen.stories.jsx
@@ -1,8 +1,16 @@
 import InboxScreen from "./InboxScreen";
+
 import store from "../lib/store";
 import { rest } from "msw";
 import { MockedState } from "./TaskList.stories";
 import { Provider } from "react-redux";
+
+import {
+  fireEvent,
+  waitFor,
+  within,
+  waitForElementToBeRemoved,
+} from "@storybook/test";
 
 export default {
   component: InboxScreen,
@@ -23,6 +31,18 @@ export const Default = {
         ),
       ],
     },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    // Waits for the component to transition from the loading state
+    await waitForElementToBeRemoved(await canvas.findByTestId("loading"));
+    // Waits for the component to be updated based on the store
+    await waitFor(async () => {
+      // Simulates pinning the first task
+      await fireEvent.click(canvas.getByLabelText("pinTask-1"));
+      // Simulates pinning the third task
+      await fireEvent.click(canvas.getByLabelText("pinTask-3"));
+    });
   },
 };
 export const Error = {
